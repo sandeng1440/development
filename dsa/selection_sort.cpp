443628f1d6fc08494ef42be175fb9a09002fd8a7
sandeng1440 <sandeng1440@gmail.com>
2024-05-16 22:01:31 +0300

    Auto commit

--- dsa/selection_sort.cpp
+++ dsa/selection_sort.cpp
@@ -3,18 +3,31 @@
 using namespace std;
 
 array<int, 10> selectionSort(array<int, 10> &);
+void selectionSort(int *, int);
 void display(array<int, 10>);
+void display(int *, int);
 array<int, 10> unsortedArr = {9, 1, 3, 0, 5, 6, 4, 8, 7, 2};
+int arr[] = {9, 1, 3, 0, 5, 6, 4, 8, 7, 2};
+int arr_size = sizeof(arr) / sizeof(int);
 
 int main() {
   display(unsortedArr);
   display(selectionSort(unsortedArr));
+  display(arr, arr_size);
+  selectionSort(arr, arr_size);
+  display(arr, arr_size);
   return 0;
 }
 
 void display(array<int, 10> arr) {
   for (int n : arr) {
     cout << n << " -> ";
+  }
+  cout << "End" << endl;
+}
+void display(int arr[], int a_len) {
+  for (int i = 0; i < a_len; i++) {
+    cout << arr[i] << " -> ";
   }
   cout << "End" << endl;
 }
@@ -34,4 +47,20 @@ array<int, 10> selectionSort(array<int, 10> &arr) {
     }
   }
   return arr;
+}
+
+void selectionSort(int arr[], int a_len) {
+  for (int i = 0; i < a_len - 1; i++) {
+    int *min = &arr[i];
+    for (int ii = i; ii < a_len; ii++) {
+      if (arr[ii] < *min) {
+        min = &arr[ii];
+      }
+    }
+    if (arr[i] != *min) {
+      int tmp = arr[i];
+      arr[i] = *min;
+      *min = tmp;
+    }
+  }
 }
