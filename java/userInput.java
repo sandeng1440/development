ad3e5f74a824e662829261132c1f8e178c43e9e9
sandeng1440 <sandeng1440@gmail.com>
2024-09-27 13:18:01 +0300

    Gitup commit

---
+++ java/userInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class myTest {
+	public static void main(String[] args){
+		Scanner c = new Scanner(System.in);
+		System.out.println("Enter int: ");
+		int n = c.nextInt(); //nextLine(), nextDouble() -> String, double | float
+		System.out.println("Num: " + n);
+	}
+}
+
