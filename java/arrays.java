30eaf3430af52c3476b6afdb26cb755a6f85561b
sandeng1440 <sandeng1440@gmail.com>
2024-10-03 17:03:51 +0300

    Gitup commit

---
+++ java/arrays.java
@@ -0,0 +1,7 @@
+public class Arrays {
+	public static void main(String[] args){
+		int[] students = {6,56,2,7,88,1};
+		int sum = students[0] + students[1];
+		System.out.println(students[0]+" + "+students[1]+ " = "+sum);
+	}
+}
