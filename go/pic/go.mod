b0c105973af0eff3fe8b969d819b569b3768696d
Santiago Deng <sandeng1440@gmail.com>
2024-12-30 15:43:39 +0300

    Gitup commit

---
+++ go/pic/go.mod
@@ -0,0 +1,5 @@
+module pic
+
+go 1.23.4
+
+require golang.org/x/tour v0.1.0
---
+++ go/stringArithmetic.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rt *rot13Reader) Read(b []byte) (int, error) {
+	n, err := rt.r.Read(b)
+	if err != nil {
+		return n, err
+	}
+	// rot13
+	for i := 0; i < n; i++ {
+		if 'a' <= b[i] && b[i] <= 'z' {
+			b[i] = byte((int(b[i]-'a')+13)%26 + 'a')
+		} else if 'A' <= b[i] && b[i] <= 'Z' {
+			b[i] = byte((int(b[i]-'A')+13)%26 + 'A')
+		}
+	}
+	return n, nil
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
